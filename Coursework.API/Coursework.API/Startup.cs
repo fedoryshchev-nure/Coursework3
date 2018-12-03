4bb2e76376c5eb83f4a653dd6fd4cd4d5682f191
Dmitriy Fedoryshchev <Dmitriy.Fedoryshchev@ibm.com>
2018-12-03 10:43:28 +0200

    Authentication to swagger added

--- Coursework.API/Coursework.API/Startup.cs
+++ Coursework.API/Coursework.API/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
+using System.Collections.Generic;
 
 namespace Coursework.API
 {
@@ -95,7 +96,22 @@ namespace Coursework.API
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1.0", new Info { Title = "OOEC API v1.0", Version = "v1.0" });
+
+                // Swagger 2.+ support
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }},
+                };
+
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+                c.AddSecurityRequirement(security);
             });
         }
 
@@ -118,14 +134,9 @@ namespace Coursework.API
             app.UseCors("EnableCORS");
 
             app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
-            // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("./swagger/v1/swagger.json", "My API V1");
-                // http://localhost:57660/swagger/v1/swagger.json json
-                // http://localhost:57660/   UI
+                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "OOEC API v1.0");
                 c.RoutePrefix = string.Empty;
             });
 
